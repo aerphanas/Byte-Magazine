5e092344075e32d160f76f317229de86b6d65449
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2023-04-12 21:24:29 +0700

    Backup script for Byte Magazine from https://vintageapple.org/byte
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

---
+++ main.lisp
@@ -0,0 +1,30 @@
+(ql:quickload '(dexador lquery alexandria))
+
+(defvar *link* "https://vintageapple.org/byte/")
+(defvar *output-file* "./url.txt")
+
+(defvar *parsed-content*
+  (lquery:$ (initialize (dex:get *link*))))
+
+(defvar *list-links*
+  (let ((vector-links (lquery:$  *parsed-content* "a" (attr :href))))
+    (mapcar (lambda (x) (concatenate 'string *link* x))
+	    (cddr (cddddr (coerce vector-links 'list))))))
+
+(defun out-to-file (path)
+  (with-open-file (stream path
+			  :direction
+			  :output
+			  :if-exists :overwrite
+			  :if-does-not-exist :create)
+    (loop
+      for i
+	in *list-links*
+      do (format stream i)
+	 (terpri stream))))
+
+(defun main ()
+  (out-to-file *output-file*)
+  (uiop:run-program
+   (format nil "wget -i ~a -P output" *output-file*)
+   :output :string))
